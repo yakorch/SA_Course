973d31f348d0c505b82c21cf5a0752aef08249a2
yakorch <korchyar19@gmail.com>
2024-05-06 21:12:12 +0300

    added container names

--- services/consul_service/service_discovery.py
+++ services/consul_service/service_discovery.py
@@ -1,4 +1,5 @@
 from services.consul_service.service_registration import consul_client
+import logging
 
 
 def discover_service(service_name: str) -> list[dict]:
@@ -10,6 +11,7 @@ def extract_URLs(services: list[dict]) -> list[str]:
     """
     Returns the list of URLs from the services.
     """
+
     URLs = []
     for service in services:
         ip_address = service["ServiceAddress"]
@@ -17,4 +19,7 @@ def extract_URLs(services: list[dict]) -> list[str]:
 
         url = f"http://{ip_address}:{port}"
         URLs.append(url)
+
+    logging.info(f"Extracted URLs: {URLs}")
+
     return URLs
